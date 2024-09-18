8465d084002f71fb0b3440517293c9e294b856f9
Vibhu Gupta <vikram.kr.gupta01@gmail.com>
2024-09-18 20:17:55 +0300

    adding some projects

---
+++ week 7/week_7.1/contextAPI/day1/src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Count/>
+     <Button/>
+    </>
+  )
+}
+
+function Count() {            
+  return <div>
+
+  </div>
+}
+
+function Button() {
+  return <div>
+
+  </div>
+}
+
+
+export default App
